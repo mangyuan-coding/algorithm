ddfd24050b3a5e2bbd2aba55ad6df679f559b41c
niumy <niumy@tebon.com.cn>
2023-11-24 18:03:04 +0800

    init

--- 11.container-with-most-water.c
+++ 11.container-with-most-water.c
@@ -4,6 +4,8 @@
  * [11] Container With Most Water
  */
 
+#include "uthash/src/uthash.h"
+
 // @lc code=start
 #include <math.h>
 
@@ -28,6 +30,67 @@
 //     return maxArea_idx(height, 0, heightSize - 1);
 // }
 
+struct node
+{
+    int left;
+    UT_hash_handle hh;
+    struct inode *inodes;
+};
+
+struct inode
+{
+    int right;
+    int val;
+    UT_hash_handle hh;
+};
+
+struct node *nodes;
+
+struct inode *find(int left, int right)
+{
+    struct inode *inodes;
+    HASH_FIND_INT(nodes, &left, inodes);
+    if (inodes != NULL)
+    {
+        struct inode *tmp_inode;
+        HASH_FIND_INT(nodes, &left, tmp_inode);
+        return tmp_inode;
+    }
+    return inodes;
+}
+
+void insert(int left, int right, int val)
+{
+    struct inode *exist_inodes;
+    HASH_FIND_INT(nodes, &left, exist_inodes);
+    if (exist_inodes != NULL)
+    {
+        struct inode *exist;
+        HASH_FIND_INT(exist_inodes, &left, exist);
+        if (exist == NULL)
+        {
+            struct inode *tmp_inode = malloc(sizeof(struct inode));
+            tmp_inode->right = right, tmp_inode->val = val;
+            HASH_ADD_INT(exist_inodes, right, tmp_inode);
+        }
+        else
+        {
+            exist->val = val;
+        }
+    }
+    else
+    {
+        struct node *tmp_node = malloc(sizeof(struct node));
+        tmp_node->left = left;
+        tmp_node->inodes = NULL;
+        HASH_ADD_INT(nodes, left, tmp_node);
+
+        struct inode *tmp_inode = malloc(sizeof(struct inode));
+        tmp_inode->right = right, tmp_inode->val = val;
+        HASH_ADD_INT(tmp_node->inodes, right, tmp_inode);
+    }
+}
+
 int maxArea(int *height, int heightSize)
 {
     if (heightSize <= 1)
@@ -35,20 +98,26 @@ int maxArea(int *height, int heightSize)
         return 0;
     }
 
-    int max_area = 0;
-    for (int i = 0; i < heightSize; i++)
+    int interval = 1;
+    while (interval < heightSize)
     {
-        for (int j = i + 1; j < heightSize; j++)
+        int limit = heightSize - interval;
+        for (int i = 0; i < limit; i++)
         {
-            int area = fmin(height[i], height[j]) * (j - i);
-            if (max_area < area)
+            int area = fmin(height[i], height[i + interval]) * interval;
+            if (interval == 1)
             {
-                max_area = area;
+                insert(i, i + interval, area);
+            }
+            else
+            {
+                insert(i, i + interval, fmax(area, fmax(find(i + 1, i + interval)->val, find(i, i + interval - 1)->val)));
             }
         }
+        interval++;
     }
 
-    return max_area;
+    return find(0, heightSize - 1)->val;
 }
 
 // @lc code=end
