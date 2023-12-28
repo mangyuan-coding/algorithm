7aa6218e6ee141f5e5ebe70a14e66bd85c133a3e
niumy <niumy@tebon.com.cn>
2023-12-28 18:39:06 +0800

    67.add-binary.c

--- 67.add-binary.c
+++ 67.add-binary.c
@@ -7,21 +7,17 @@
 // @lc code=start
 #include <string.h>
 #include <stdlib.h>
-#include <stdio.h>
 #include <stdbool.h>
 #include <math.h>
 
-const unsigned long long mask = 0x8000000000000000;
+const unsigned long long mask = 0x80000000;
 
 char *addBinary(char *a, char *b)
 {
     int len_a = strlen(a);
     int len_b = strlen(b);
 
-    // 10000 / 63 = 158.73
-    // 159
-    // 64 * 159 = 10176
-    int bit_size_base = ((fmax(len_a, len_b) + 1) / 63) + 1;
+    int bit_size_base = ((fmax(len_a, len_b) + 1) / 31) + 1;
     unsigned long long *bit_a = (unsigned long long *)malloc(sizeof(unsigned long long *) * bit_size_base);
     unsigned long long *bit_b = (unsigned long long *)malloc(sizeof(unsigned long long *) * bit_size_base);
     unsigned long long *bit_c = (unsigned long long *)malloc(sizeof(unsigned long long *) * bit_size_base);
@@ -32,18 +28,35 @@ char *addBinary(char *a, char *b)
         bit_c[i] = 0x0;
     }
 
-    for (int i = 0; i < len_a; i++)
+    int cur = bit_size_base;
+    unsigned long long bit = 0x1;
+    for (int i = len_a - 1, j = 0; i >= 0; i--, j++)
     {
-        int idx = i / 63;
-        bit_a[bit_size_base - idx - 1] <<= 1;
-        bit_a[bit_size_base - idx - 1] += a[i] - '0';
+        if (j % 31 == 0)
+        {
+            cur--;
+            bit = 0x1;
+        }
+        if (a[i] == '1')
+        {
+            bit_a[cur] |= bit;
+        }
+        bit <<= 1;
     }
 
-    for (int i = 0; i < len_b; i++)
+    cur = bit_size_base;
+    for (int i = len_b - 1, j = 0; i >= 0; i--, j++)
     {
-        int idx = i / 63;
-        bit_b[bit_size_base - idx - 1] <<= 1;
-        bit_b[bit_size_base - idx - 1] += b[i] - '0';
+        if (j % 31 == 0)
+        {
+            cur--;
+            bit = 0x1;
+        }
+        if (b[i] == '1')
+        {
+            bit_b[cur] |= bit;
+        }
+        bit <<= 1;
     }
 
     int ret_size = 0;
@@ -51,29 +64,20 @@ char *addBinary(char *a, char *b)
 
     for (int i = bit_size_base - 1; i >= 0; i--)
     {
-        printf("bit_a[%d] is 0x%llx \n", i, bit_a[i]);
-        printf("bit_b[%d] is 0x%llx \n", i, bit_b[i]);
         bit_c[i] = bit_a[i] + bit_b[i] + carry;
-        carry = mask & bit_c[i] > 0 ? 0x1 : 0x0;
-        printf("bit_c[%d] is 0x%llx \n", i, bit_c[i]);
+        carry = (mask & bit_c[i]) > 0 ? 0x1 : 0x0;
     }
 
     char *ret = (char *)malloc(sizeof(char) * (fmax(len_a, len_b) + 2));
     int idx = 0;
-
+    bool start = false;
+    bit = 0x40000000;
     for (int i = 0; i < bit_size_base; i++)
     {
-        if (idx == 0 && bit_c[i] == 0)
+        for (int j = 1; j < 32; j++)
         {
-            continue;
-        }
-        bit_c[i] <<= 1;
-
-        bool start = false;
-        for (int j = 1; j < 64; j++)
-        {
-            bit_c[i] <<= 1;
-            bool zero = (mask & bit_c[i]) == 0;
+            bool zero = (bit & bit_c[i]) == 0;
+            bit >>= 1;
             if (zero && !start && idx == 0)
             {
                 continue;
@@ -81,6 +85,7 @@ char *addBinary(char *a, char *b)
             ret[idx++] = zero ? '0' : '1';
             start = true;
         }
+        bit = 0x40000000;
     }
     if (idx == 0)
     {
@@ -90,13 +95,15 @@ char *addBinary(char *a, char *b)
     return ret;
 }
 // @lc code=end
+#include <stdio.h>
 
 int main(int argc, char const *argv[])
 {
 
-    printf("       answer is %s \n", addBinary("10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101", "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011"));
-    printf("except answer is %s \n", "110111101100010011000101110110100000011101000101011001000011011000001100011110011010010011000000000");
-    printf("            a is     %s \n", "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101");
-    printf("            b is %s \n", "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011");
+    printf("       answer is %s \n", addBinary("1000111111101100010001101101100000011100111001010110000010100111010111000101010001",
+                                               "110110101011011101110100000010101111111110011100011011011000110000101010010010010101000110"));
+    printf("except answer is %s \n", "110110110100011101100000010100011101011110111001010100101110110011010001101001011010010111");
+    printf("            a is     %s \n", "1000111111101100010001101101100000011100111001010110000010100111010111000101010001");
+    printf("            b is %s \n", "110110101011011101110100000010101111111110011100011011011000110000101010010010010101000110");
     return 0;
 }
