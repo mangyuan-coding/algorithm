c3fab1fb980c38929c726cc72cfba8b884204731
niumy <niumy@tebon.com.cn>
2024-03-14 15:25:06 +0800

    102.binary-tree-level-order-traversal.c

---
+++ 102.binary-tree-level-order-traversal.c
@@ -0,0 +1,122 @@
+/*
+ * @lc app=leetcode.cn id=102 lang=c
+ *
+ * [102] Binary Tree Level Order Traversal
+ */
+#include <stdio.h>
+#include "include/type.h"
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+#include <stdlib.h>
+
+const int MAXIMUM = 2001;
+
+int **levelOrder(struct TreeNode *root, int *returnSize, int **returnColumnSizes)
+{
+
+    *returnSize = 0;
+    if (root == NULL)
+    {
+        return NULL;
+    }
+
+    int **order = (int **)malloc(sizeof(int *) * MAXIMUM);
+    *returnColumnSizes = (int *)malloc(sizeof(int) * MAXIMUM);
+
+    struct TreeNode **pre_queue = (struct TreeNode **)malloc(sizeof(struct TreeNode *)), **cur_queue;
+
+    int pre_size = 1, cur_size = 0;
+    pre_queue[pre_size - 1] = root;
+
+    *returnSize = 1;
+    (*returnColumnSizes)[0] = 1;
+    order[0] = (int *)malloc(sizeof(int));
+    order[0][0] = root->val;
+
+    int deep = 2;
+    while (1)
+    {
+        if (pre_size == 0)
+        {
+            break;
+        }
+        // printf("Mark 1 %d start \n", *returnSize);
+        (*returnSize)++;
+        order[*returnSize - 1] = (int *)malloc(sizeof(int) * pre_size * 2);
+        cur_size = 0;
+        cur_queue = (struct TreeNode **)malloc(sizeof(struct TreeNode *) * pre_size * 2);
+        // first put cur
+        for (int i = 0; i < pre_size; i++)
+        {
+            struct TreeNode *node = pre_queue[i];
+            if (node->left != NULL)
+            {
+                cur_queue[cur_size++] = node->left;
+                order[*returnSize - 1][cur_size - 1] = node->left->val;
+            }
+            if (node->right != NULL)
+            {
+                cur_queue[cur_size++] = node->right;
+                order[*returnSize - 1][cur_size - 1] = node->right->val;
+            }
+        }
+        (*returnColumnSizes)[*returnSize - 1] = cur_size;
+        // printf("Mark 1 %d end \n", *returnSize);
+        free(pre_queue);
+        pre_size = cur_size;
+        pre_queue = cur_queue;
+    }
+    (*returnSize)--;
+    return order;
+}
+// @lc code=end
+
+int main(int argc, char const *argv[])
+{
+    struct TreeNode *root = (struct TreeNode *)malloc(sizeof(struct TreeNode *));
+    root->val = 3;
+    root->left = (struct TreeNode *)malloc(sizeof(struct TreeNode *));
+    root->left->val = 9;
+    root->right = (struct TreeNode *)malloc(sizeof(struct TreeNode *));
+    root->right->val = 20;
+    root->left->left = NULL;
+    root->left->right = NULL;
+    root->right->left = (struct TreeNode *)malloc(sizeof(struct TreeNode *));
+    root->right->left->val = 15;
+    root->right->right = (struct TreeNode *)malloc(sizeof(struct TreeNode *));
+    root->right->right->val = 7;
+    root->right->left->left = NULL;
+    root->right->left->right = NULL;
+    root->right->right->left = NULL;
+    root->right->right->right = NULL;
+    int *returnSize = (int *)malloc(sizeof(int));
+    int **returnColumnSizes = (int **)malloc(sizeof(int *));
+
+    int **order = levelOrder(root, returnSize, returnColumnSizes);
+
+    printf("[");
+    for (int i = 0; i < *returnSize; i++)
+    {
+        printf("[");
+        for (int j = 0; j < (*returnColumnSizes)[i]; j++)
+        {
+            printf("%d,", order[i][j]);
+        }
+        printf("],");
+    }
+    printf("]");
+    return 0;
+}
