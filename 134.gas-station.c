ec9d7b32caf16d79a9fbae0a1ca5675b3e3fd0e1
niumy <niumy@tebon.com.cn>
2024-03-22 18:05:56 +0800

    zc

---
+++ 134.gas-station.c
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode.cn id=134 lang=c
+ *
+ * [134] Gas Station
+ */
+
+// @lc code=start
+#include <stdlib.h>
+
+int canCompleteCircuit(int *gas, int gasSize, int *cost, int costSize)
+{
+    int start = 0;
+    while (start < gasSize)
+    {
+        int cur_gas = gas[start] - cost[start];
+        if (cur_gas < 0)
+        {
+            start++;
+            continue;
+        }
+        int travel = (start == gasSize - 1) ? 0 : start + 1;
+        while (travel != start)
+        {
+            cur_gas += (gas[travel] - cost[travel]);
+            if (cur_gas < 0)
+            {
+                break;
+            }
+            else if (travel == gasSize - 1)
+            {
+                travel = 0;
+            }
+            else
+            {
+                travel++;
+            }
+        }
+
+        if (travel == start)
+        {
+            return start;
+        }
+
+        start = travel + 1;
+    }
+
+    return -1;
+}
+// @lc code=end
